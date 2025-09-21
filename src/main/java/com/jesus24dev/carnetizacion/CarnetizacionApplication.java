2b62624df18cfa6324c1e9850d68f41a2e41c58b
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-20 21:50:59 -0400

    first version

---
+++ src/main/java/com/jesus24dev/carnetizacion/CarnetizacionApplication.java
@@ -0,0 +1,13 @@
+package com.jesus24dev.carnetizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesus24dev/carnetizacion/CarnetizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus24dev.carnetizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
