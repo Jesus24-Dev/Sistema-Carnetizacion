ebeefd897ae98fd06fa7ae717e3b4484ef12719f
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-22 22:04:42 -0400

    created object response for employee

---
+++ src/main/java/com/jesus24dev/carnetizacion/dto/response/EmployeeResponse.java
@@ -0,0 +1,55 @@
+
+package com.jesus24dev.carnetizacion.dto.response;
+
+import com.jesus24dev.carnetizacion.models.Employee;
+import java.time.LocalDate;
+
+public class EmployeeResponse {
+    private final String ci;
+    private final String name;
+    private final String lastname;
+    private final Employee.Gender gender;
+    private final String email;
+    private final LocalDate birthday;
+    private final Long imageId;
+    
+    public EmployeeResponse(Employee employee) {
+        this.ci = employee.getCi();
+        this.name = employee.getName();
+        this.lastname = employee.getLastname();
+        this.gender = employee.getGender();
+        this.email = employee.getEmail();
+        this.birthday = employee.getBirthday();
+        this.imageId = (employee.getImage() != null) ? employee.getImage().getId() : null;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Employee.Gender getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public Long getImageId() {
+        return imageId;
+    }
+    
+    
+}
