b77d2ddfe891ec4c55282a3a31f237f14f204b3f
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-26 01:10:21 -0400

    created simple service for uploades images

---
+++ src/main/java/com/jesus24dev/carnetizacion/services/ImagesService.java
@@ -0,0 +1,37 @@
+
+package com.jesus24dev.carnetizacion.services;
+
+import com.jesus24dev.carnetizacion.models.Employee;
+import com.jesus24dev.carnetizacion.models.Images;
+import com.jesus24dev.carnetizacion.repository.ImagesRepository;
+import java.nio.file.Path;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImagesService {
+    
+    private ImagesRepository imagesRepository;
+    private EmployeeService employeeService;
+    
+    @Autowired
+    public ImagesService(ImagesRepository imagesRepository, EmployeeService employeeService){
+        this.imagesRepository = imagesRepository;
+        this.employeeService = employeeService;
+    }
+    
+    public Images createImage(Path path, String ci){
+        Images image = new Images();
+        Employee employeeFounded = employeeService.findEmployeeByCi(ci);
+        
+        image.setPathFile(path.toString());
+        image.setEmployee(employeeFounded);
+        
+        employeeFounded.setImage(image);
+        
+        employeeService.updateImage(image, employeeFounded);
+        imagesRepository.save(image);
+        
+        return image;
+    }
+}
