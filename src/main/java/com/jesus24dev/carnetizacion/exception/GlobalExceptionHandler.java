c176af926e7a6e9830f9e11b21ad05ad7fc9ef0f
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-26 00:57:34 -0400

    add exceptions for uploaded files

--- src/main/java/com/jesus24dev/carnetizacion/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jesus24dev/carnetizacion/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +24,17 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("error", "Unexpected error: " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+    
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body("El archivo es demasiado grande");
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<String> handleMultipartException(MultipartException exc) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error en la solicitud multipart");
     }
 }
