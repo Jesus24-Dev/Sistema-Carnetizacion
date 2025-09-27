e333bb5251a4b1e9308fe6e848cee5e1ef111e34
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-27 19:18:35 -0400

    add existsByCi method to EmployeeRepository for CI existence check

--- src/main/java/com/jesus24dev/carnetizacion/repository/EmployeeRepository.java
+++ src/main/java/com/jesus24dev/carnetizacion/repository/EmployeeRepository.java
@@ -5,5 +5,5 @@ import com.jesus24dev.carnetizacion.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String>{
-  
+  boolean existsByCi(String ci);
 }
