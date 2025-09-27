048a521854ee26d1e082ddf50cd3a3ea61539e7b
Jesus24-Dev <siritjesus24@gmail.com>
2025-09-27 19:19:05 -0400

    add findByEmployeeCi method to ImagesRepository for retrieving images by employee CI

--- src/main/java/com/jesus24dev/carnetizacion/repository/ImagesRepository.java
+++ src/main/java/com/jesus24dev/carnetizacion/repository/ImagesRepository.java
@@ -2,9 +2,11 @@
 package com.jesus24dev.carnetizacion.repository;
 
 import com.jesus24dev.carnetizacion.models.Images;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImagesRepository extends JpaRepository<Images, UUID> {
     void deleteByEmployeeCi(String ci);
+    Optional<Images> findByEmployeeCi(String ci);
 }
