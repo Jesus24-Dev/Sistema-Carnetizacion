78469d6accfad79ac1decd509836eaf1bf4fa340
Jesus24-Dev <siritjesus24@gmail.com>
2025-10-01 12:32:59 -0400

    Refactor AuthController to implement home method with CI parameter handling and model attribute for carnet status

--- src/main/java/com/jesus24dev/carnetizacion/controllers/AuthController.java
+++ src/main/java/com/jesus24dev/carnetizacion/controllers/AuthController.java
@@ -2,14 +2,22 @@
 package com.jesus24dev.carnetizacion.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class AuthController {
-    @GetMapping
-    public String index(){
+    @GetMapping("/")
+    public String home(@RequestParam(value = "ci", required = false) String ci, Model model) {
+        if (ci != null && !ci.trim().isEmpty()) {
+            // Lógica para buscar estado del carnet
+            //TODO hacer servicio para verificar estado de solicitud.
+//            String estado = carnetService.obtenerEstadoCarnet(ci);
+            model.addAttribute("carnetStatus", "APROBADO");
+        }
         return "index";
     }
     
